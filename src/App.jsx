2c0f6dacaa421a5867d623edcc00fe724e033eb7
yaredow <yaredyilma11@gmail.com>
2023-08-07 16:15:33 +0300

    create a request and action

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,47 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "../src/ui/Home";
+import Menu, { loader as menuLoader } from "../src/features/menu/Menu";
+import Cart from "../src/features/cart/Cart";
+import CreateOrder, {
+  action as createOrderAction,
+} from "../src/features/order/CreateOrder";
+import Order, { loader as orderLoader } from "../src/features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+      },
+    ],
+  },
+]);
+
 export default function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Form } from "react-router-dom";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -36,9 +37,9 @@ function CreateOrder() {
 
   return (
     <div>
-      <h2>Ready to order? Let's go!</h2>
+      <h2>Ready to order? Lets go!</h2>
 
-      <form>
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -72,9 +73,16 @@ function CreateOrder() {
         <div>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  console.log(formData);
+
+  return null;
 }
 
 export default CreateOrder;
